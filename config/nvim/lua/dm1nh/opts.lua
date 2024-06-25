b3af3924c8a7207b42de758e6e941f6620fbe5b6
dangminhngo <dangminhngo.dev@gmail.com>
2024-06-25 12:59:00 +0700

    fix: disable indent options

--- config/nvim/lua/dm1nh/opts.lua
+++ config/nvim/lua/dm1nh/opts.lua
@@ -17,8 +17,8 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-vim.opt.breakindent = true
-vim.opt.smartindent = true
+vim.opt.breakindent = false
+vim.opt.smartindent = false
 
 vim.opt.wrap = false
 
