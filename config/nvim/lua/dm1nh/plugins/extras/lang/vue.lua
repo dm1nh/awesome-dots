9d7056eeadb85075b06c8dd8efae03e9393a8af4
dm1nh <dangminhngo.dev@gmail.com>
2024-07-10 11:16:03 +0700

    fix(nvim,vue): run volar in hybrid mode

--- config/nvim/lua/dm1nh/plugins/extras/lang/vue.lua
+++ config/nvim/lua/dm1nh/plugins/extras/lang/vue.lua
@@ -16,7 +16,13 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        volar = {},
+        volar = {
+          init_options = {
+            vue = {
+              hybridMode = true,
+            },
+          },
+        },
         vtsls = {},
       },
     },
